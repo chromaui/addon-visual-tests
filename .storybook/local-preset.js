22f499d651c4349c6b7838028a65af5eca18acd6
Tom Coleman <tom@chromatic.com>
2023-06-06 11:45:03 +1000

    Switch to use node API defined in CLI

--- .storybook/local-preset.js
+++ .storybook/local-preset.js
@@ -1,3 +1,6 @@
+import express from "express";
+import { run } from "chromatic/node";
+
 /**
  * to load the built addon in this test Storybook
  */
@@ -8,3 +11,44 @@ function managerEntries(entry = []) {
 module.exports = {
   managerEntries,
 };
+
+function serve() {
+  // Temporarily create a server as a psuedo server channel. This is throwaway code
+  const app = express();
+
+  app.post("/build", async (req, res) => {
+    let sent = false;
+    res.setHeader("access-control-allow-origin", "*");
+
+    await run({
+      flags: {
+        projectToken: "00baf09dbbe8",
+      },
+      options: {
+        onTaskComplete(ctx) {
+          console.log(`Completed task '${ctx.title}'`);
+          if (ctx.announcedBuild && !sent) {
+            const { id, number, app } = ctx.announcedBuild;
+            res.json({
+              id,
+              url: `https://www.chromatic.com/build?appId=${app.id}&number=${number}`,
+            });
+            sent = true;
+          }
+        },
+      },
+    });
+  });
+
+  app
+    .listen(8765, () => {
+      console.log("Listening on port 8765");
+    })
+    .on("error", (err) => {
+      // The above will throw when you try and build your SB or start a second dev server.
+      // As this is throwaway code we won't try and fix this.
+      console.log(`caught err, ${err}`);
+    });
+}
+
+serve();
