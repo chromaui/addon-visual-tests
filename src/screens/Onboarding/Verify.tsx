32ce8e5af2b6f337681d35ffb54091cc8c060dd2
Gert Hengeveld <info@ghengeveld.nl>
2023-06-08 15:42:05 +0200

    Open Chromatic in dialog and auto-close when authenticated

--- src/screens/Onboarding/Verify.tsx
+++ src/screens/Onboarding/Verify.tsx
@@ -33,27 +33,50 @@ interface VerifyProps {
   verificationUrl: string;
 }
 
-export const Verify = ({ onBack, userCode, verificationUrl }: VerifyProps) => (
-  <Container>
-    <BackButton onClick={onBack}>
-      <BackIcon />
-      Back
-    </BackButton>
-    <Stack>
-      <div>
-        <Heading>Verify your account</Heading>
-        <Text>
-          Enter this verification code on Chromatic to grant access to your published Storybooks.
-        </Text>
-        <Digits>
-          {userCode?.split("").map((char: string, index: number) => (
-            <li key={index}>{char.replace(/[^A-Z0-9]/, "")}</li>
-          ))}
-        </Digits>
-      </div>
-      <Button isLink appearance="secondary" href={verificationUrl} target="_blank">
-        Go to Chromatic
-      </Button>
-    </Stack>
-  </Container>
-);
+export const Verify = ({ onBack, userCode, verificationUrl }: VerifyProps) => {
+  const dialog = React.useRef<Window>();
+
+  // Close the dialog window when the screen gets unmounted.
+  React.useEffect(() => () => dialog.current?.close(), []);
+
+  const openChromatic = () => {
+    const width = 800;
+    const height = 800;
+    const usePopup = window.innerWidth > width && window.innerHeight > height;
+
+    if (usePopup) {
+      const left = (window.innerWidth - width) / 2 + window.screenLeft;
+      const top = (window.innerHeight - height) / 2 + window.screenTop;
+      const options = `scrollbars=yes,width=${width},height=${height},top=${top},left=${left}`;
+      dialog.current = window.open(verificationUrl, "oauth-dialog", options);
+      if (window.focus) dialog.current.focus();
+    } else {
+      dialog.current = window.open(verificationUrl, "_blank");
+    }
+  };
+
+  return (
+    <Container>
+      <BackButton onClick={onBack}>
+        <BackIcon />
+        Back
+      </BackButton>
+      <Stack>
+        <div>
+          <Heading>Verify your account</Heading>
+          <Text>
+            Enter this verification code on Chromatic to grant access to your published Storybooks.
+          </Text>
+          <Digits>
+            {userCode?.split("").map((char: string, index: number) => (
+              <li key={index}>{char.replace(/[^A-Z0-9]/, "")}</li>
+            ))}
+          </Digits>
+        </div>
+        <Button appearance="secondary" onClick={openChromatic}>
+          Go to Chromatic
+        </Button>
+      </Stack>
+    </Container>
+  );
+};
