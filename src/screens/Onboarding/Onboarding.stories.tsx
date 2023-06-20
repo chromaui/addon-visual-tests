69b1b2b2eef75ef34db321699e1696f51c53ff7a
Gert Hengeveld <info@ghengeveld.nl>
2023-06-20 12:54:15 +0200

    Fix type import

--- src/screens/Onboarding/Onboarding.stories.tsx
+++ src/screens/Onboarding/Onboarding.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from "@storybook/addon-actions";
-import { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { findByRole, userEvent } from "@storybook/testing-library";
 import { rest } from "msw";
 
