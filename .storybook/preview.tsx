a4f2299b0977fdee0605e8f7aee4d9aafb2b7ce1
Gert Hengeveld <info@ghengeveld.nl>
2023-07-14 09:57:18 +0200

    Fix formatting

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,12 @@
-import { Global, ThemeProvider, themes, createReset, convert, styled, useTheme } from "@storybook/theming";
+import {
+  Global,
+  ThemeProvider,
+  themes,
+  createReset,
+  convert,
+  styled,
+  useTheme,
+} from "@storybook/theming";
 import type { Preview } from "@storybook/react";
 import { initialize, mswDecorator } from "msw-storybook-addon";
 import React from "react";
@@ -7,40 +15,45 @@ import React from "react";
 initialize({
   onUnhandledRequest(req) {
     if (req.url.origin !== document.location.origin) {
-      console.error(`[MSW] %s %s %s (UNHANDLED)`, new Date().toTimeString().slice(0, 8), req.method.toUpperCase(), req.url.href);
+      console.error(
+        `[MSW] %s %s %s (UNHANDLED)`,
+        new Date().toTimeString().slice(0, 8),
+        req.method.toUpperCase(),
+        req.url.href
+      );
     }
-  }
+  },
 });
 const Panels = styled.div<{
   orientation: "right" | "bottom";
-}>(({
-  orientation
-}) => ({
-  flexDirection: orientation === "right" ? "row" : "column"
-}), {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: 40,
-  margin: 40
-});
+}>(
+  ({ orientation }) => ({
+    flexDirection: orientation === "right" ? "row" : "column",
+  }),
+  {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 40,
+    margin: 40,
+  }
+);
 const Panel = styled.div<{
   orientation: "right" | "bottom";
-}>(({
-  orientation
-}) => ({
-  width: orientation === "right" ? 420 : 880,
-  height: orientation === "right" ? 640 : 300,
-  overflow: "auto"
-}), ({
-  theme
-}) => ({
-  position: "relative",
-  outline: `1px solid ${theme.color.border}`,
-  background: theme.background.content,
-  color: theme.color.defaultText,
-  fontSize: theme.typography.size.s2 - 1
-}));
+}>(
+  ({ orientation }) => ({
+    width: orientation === "right" ? 420 : 880,
+    height: orientation === "right" ? 640 : 300,
+    overflow: "auto",
+  }),
+  ({ theme }) => ({
+    position: "relative",
+    outline: `1px solid ${theme.color.border}`,
+    background: theme.background.content,
+    color: theme.color.defaultText,
+    fontSize: theme.typography.size.s2 - 1,
+  })
+);
 const ThemedSetRoot = () => {
   const theme = useTheme();
   React.useEffect(() => {
@@ -55,57 +68,61 @@ export const decorators = [
   mswDecorator,
   // Render two panels side-by-side or stacked, depending on selected orientation
   // Note this assumes any play functions are equipped to deal with multiple canvases
-  (StoryFn, {
-    globals,
-    parameters
-  }) => {
+  (StoryFn, { globals, parameters }) => {
     const theme = globals.theme || parameters.theme || "right";
-    return theme === "light" || theme === "dark" ? <ThemeProvider theme={convert(themes[theme])}>
-      <Global styles={createReset} />
-      <Global styles={{
-        "#storybook-root": {
-          height: "100vh",
-          padding: 0
-        }
-      }}></Global>
-      <ThemedSetRoot />
-      <StoryFn />
-    </ThemeProvider> : <>
-      <ThemeProvider theme={convert(themes.light)}>
+    return theme === "light" || theme === "dark" ? (
+      <ThemeProvider theme={convert(themes[theme])}>
         <Global styles={createReset} />
+        <Global
+          styles={{
+            "#storybook-root": {
+              height: "100vh",
+              padding: 0,
+            },
+          }}
+        ></Global>
         <ThemedSetRoot />
+        <StoryFn />
       </ThemeProvider>
-      <Panels orientation={theme}>
+    ) : (
+      <>
         <ThemeProvider theme={convert(themes.light)}>
-          <Panel orientation={theme} data-canvas={theme}>
-            <StoryFn />
-          </Panel>
+          <Global styles={createReset} />
+          <ThemedSetRoot />
         </ThemeProvider>
-        <ThemeProvider theme={convert(themes.dark)}>
-          <Panel orientation={theme} data-canvas={theme}>
-            <StoryFn />
-          </Panel>
-        </ThemeProvider>
-      </Panels>
-    </>;
-  }];
+        <Panels orientation={theme}>
+          <ThemeProvider theme={convert(themes.light)}>
+            <Panel orientation={theme} data-canvas={theme}>
+              <StoryFn />
+            </Panel>
+          </ThemeProvider>
+          <ThemeProvider theme={convert(themes.dark)}>
+            <Panel orientation={theme} data-canvas={theme}>
+              <StoryFn />
+            </Panel>
+          </ThemeProvider>
+        </Panels>
+      </>
+    );
+  },
+];
 export const parameters: Preview["parameters"] = {
   actions: {
-    argTypesRegex: "^on[A-Z].*"
+    argTypesRegex: "^on[A-Z].*",
   },
   backgrounds: {
-    disable: true
+    disable: true,
   },
   chromatic: {
-    viewports: [960]
+    viewports: [960],
   },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/
-    }
+      date: /Date$/,
+    },
   },
-  layout: "fullscreen"
+  layout: "fullscreen",
 };
 export const globalTypes = {
   theme: {
@@ -114,23 +131,28 @@ export const globalTypes = {
     toolbar: {
       icon: "sidebaralt",
       title: "Theme",
-      items: [{
-        value: "light",
-        icon: "circlehollow",
-        title: "Light"
-      }, {
-        value: "dark",
-        icon: "circle",
-        title: "Dark"
-      }, {
-        value: "right",
-        icon: "sidebaralt",
-        title: "Right 2-up"
-      }, {
-        value: "bottom",
-        icon: "bottombar",
-        title: "Bottom 2-up"
-      }]
-    }
-  }
+      items: [
+        {
+          value: "light",
+          icon: "circlehollow",
+          title: "Light",
+        },
+        {
+          value: "dark",
+          icon: "circle",
+          title: "Dark",
+        },
+        {
+          value: "right",
+          icon: "sidebaralt",
+          title: "Right 2-up",
+        },
+        {
+          value: "bottom",
+          icon: "bottombar",
+          title: "Bottom 2-up",
+        },
+      ],
+    },
+  },
 };
