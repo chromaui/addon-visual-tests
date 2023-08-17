dd82bf834b291b4b867b40fd1278fb430d165795
Tom Coleman <tom@chromatic.com>
2023-08-17 10:47:41 +1000

    Fix type shenanigans

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,8 +2,10 @@ import { Badge as BaseBadge } from "@storybook/components";
 import { styled } from "@storybook/theming";
 import { ReactNode } from "react";
 
+type BadgeProps = React.ComponentProps<typeof BaseBadge>;
+
 // NOTE: The `Badge` exported by `@storybook/components` incorrectly isn't typed to accept children
-export const Badge = styled(BaseBadge)<{ children?: ReactNode }>({
+export const Badge: React.FC<BadgeProps & { children?: ReactNode }> = styled(BaseBadge)({
   padding: "4px 8px",
   margin: "0 6px",
 });
