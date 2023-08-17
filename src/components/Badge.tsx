2e2cf8d61aabb2a9638ab49386b7a1af1dbc3b97
Tom Coleman <tom@chromatic.com>
2023-08-17 10:36:01 +1000

    Fix all broken types

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,7 +2,8 @@ import { Badge as BaseBadge } from "@storybook/components";
 import { styled } from "@storybook/theming";
 import { ReactNode } from "react";
 
-export const Badge: typeof BaseBadge = styled(BaseBadge)<{ children?: ReactNode }>({
+// NOTE: The `Badge` exported by `@storybook/components` incorrectly isn't typed to accept children
+export const Badge = styled(BaseBadge)<{ children?: ReactNode }>({
   padding: "4px 8px",
   margin: "0 6px",
 });
