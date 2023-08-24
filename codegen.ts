49291ba54519b609b909f1a2d19d98405f8cc21f
Gert Hengeveld <info@ghengeveld.nl>
2023-08-24 09:37:48 +0200

    Use local schema file for graphql-codegen

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "https://www.dev-chromatic.com/api",
+  schema: "src/gql/public-schema.graphql",
   documents: ["src/**/*.tsx"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
