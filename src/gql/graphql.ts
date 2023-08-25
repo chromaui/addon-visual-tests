ec43171a689bc805637b19ccbceb6ed513a0ae1a
Gert Hengeveld <info@ghengeveld.nl>
2023-08-25 10:34:48 +0200

    Update selectedTest whenever the input tests change

--- src/gql/graphql.ts
+++ src/gql/graphql.ts
@@ -761,13 +761,11 @@ export type ProjectBranchNamesArgs = {
 
 export type ProjectLastBuildArgs = {
   branches?: InputMaybe<Array<Scalars['String']['input']>>;
-  commit?: InputMaybe<Scalars['String']['input']>;
   defaultBranch?: InputMaybe<Scalars['Boolean']['input']>;
   localBuilds?: InputMaybe<LocalBuildsSpecifierInput>;
   results?: InputMaybe<Array<BuildResult>>;
   slug?: InputMaybe<Scalars['String']['input']>;
   statuses?: InputMaybe<Array<BuildStatus>>;
-  uncommittedHash?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type PublicAccountInfo = {
