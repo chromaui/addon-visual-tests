4037ca492b9c8dabed88729799986718421c44d6
Gert Hengeveld <info@ghengeveld.nl>
2023-08-25 10:46:16 +0200

    Add instructions on how to update the GraphQL schema file

--- codegen.ts
+++ codegen.ts
@@ -1,6 +1,8 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
+  // @see https://github.com/chromaui/chromatic/blob/main/lib/schema/public-schema.graphql
+  // We don't automatically update the schema yet, we copy it manually whenever it changes.
   schema: "src/gql/public-schema.graphql",
   documents: ["src/**/*.tsx"],
   ignoreNoDocuments: true, // for better experience with the watcher
