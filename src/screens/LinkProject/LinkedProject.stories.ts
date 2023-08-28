f9f5238f170573914c6f39f5a13e77099c29e03c
Tom Coleman <tom@chromatic.com>
2023-08-28 13:59:20 +1000

    Move `LinkedProject` story into its own file

---
+++ src/screens/LinkProject/LinkedProject.stories.ts
@@ -0,0 +1,49 @@
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
+import { graphql } from "msw";
+
+import { ProjectQueryQuery } from "../../gql/graphql";
+import { storyWrapper } from "../../utils/graphQLClient";
+import { withFigmaDesign } from "../../utils/withFigmaDesign";
+import { LinkedProject } from "./LinkedProject";
+
+const withGraphQLQuery = (...args: Parameters<typeof graphql.query>) => ({
+  msw: {
+    handlers: [graphql.query(...args)],
+  },
+});
+
+const meta = {
+  component: LinkedProject,
+  args: {
+    projectId: "Project:abc123",
+    goToNext: action("goToNext"),
+    setAccessToken: action("setAccessToken"),
+  },
+  decorators: [storyWrapper],
+  parameters: {
+    ...withGraphQLQuery("ProjectQuery", (req, res, ctx) =>
+      res(
+        ctx.data({
+          project: {
+            id: "789",
+            name: "acme",
+            webUrl: "https://www.chromatic.com/builds?appId=789",
+            lastBuild: {
+              branch: "main",
+              number: 123,
+            },
+          },
+        } satisfies ProjectQueryQuery)
+      )
+    ),
+    ...withFigmaDesign(
+      "https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=508-317094&t=435fylbu7gUQNEgq-4"
+    ),
+  },
+} satisfies Meta<typeof LinkedProject>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
