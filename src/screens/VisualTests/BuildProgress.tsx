aa8606a8228350b485f14a4d35b18024f70696a8
Matthew Weeks <weeksling@gmail.com>
2023-09-04 21:02:46 -0400

    Combine message map and steps weights into one array

--- src/screens/VisualTests/BuildProgress.tsx
+++ src/screens/VisualTests/BuildProgress.tsx
@@ -31,41 +31,52 @@ type BuildProgressProps = {
   buildProgress: BuildProgressPayload;
 };
 
-const messageMap: Record<BuildProgressPayload["step"], (payload: BuildProgressPayload) => string> =
-  {
-    initialize: () => `📦 Validating Storybook files...`,
-    build: () => `📦 Validating Storybook files...`,
-    upload: (payload) => `📡 Uploading to Chromatic ${payload.progress}/${payload.total}...`, // TODO represent progress in bytes
-    verify: () => `🛠️ Initiating build`, // TODO build number
-    snapshot: (payload) => `👀 Running visual test ${payload.progress}/${payload.total}...`, // TODO count
-    complete: () => "🎉 Visual tests completed!",
-  };
-// TODO: In the design, we have a starting state, a progress state, and a completed state for each step. For now, we only have the progress state. https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2303-373686&mode=design&t=9s23AuWqjDaCTdXg-4
-
 // Weight to take a percentage of the progress bar
 // Using an array to preserve order
-const stepWeightArray = [
-  { step: "initialize", weight: 0 },
-  { step: "build", weight: 40 },
-  { step: "upload", weight: 10 },
-  { step: "verify", weight: 10 },
-  { step: "snapshot", weight: 40 },
-  { step: "complete", weight: 0 },
-];
+const stepsArray: Array<{
+  step: BuildProgressPayload["step"];
+  weight: number;
+  message: (payload: BuildProgressPayload) => string;
+}> = [
+    { step: "initialize", weight: 0, message: () => `📦 Validating Storybook files...` },
+    { step: "build", weight: 40, message: () => `📦 Validating Storybook files...` },
+    {
+      step: "upload",
+      weight: 10,
+      message: (payload) => `📡 Uploading to Chromatic ${payload.progress}/${payload.total}...`,
+    },
+    { step: "verify", weight: 10, message: () => `🛠️ Initiating build` },
+    {
+      step: "snapshot",
+      weight: 40,
+      message: (payload) => `👀 Running visual test ${payload.progress}/${payload.total}...`,
+    },
+    { step: "complete", weight: 0, message: () => "🎉 Visual tests completed!" },
+  ];
+// TODO: In the design, we have a starting state, a progress state, and a completed state for each step. For now, we only have the progress state. https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2303-373686&mode=design&t=9s23AuWqjDaCTdXg-4Z3ZQ
 
-const totalWeight = stepWeightArray.reduce((acc, { weight }) => acc + weight, 0);
+const totalWeight = stepsArray.reduce((acc, { weight }) => acc + weight, 0);
 
 export function BuildProgress({ buildProgress }: BuildProgressProps) {
   const { step } = buildProgress;
-  const stepWeight = stepWeightArray.find((s) => s.step === step)?.weight;
-  const stepIndex = stepWeightArray.findIndex((s) => s.step === step);
-  const previousStepsWeight = stepWeightArray.slice(0, stepIndex).reduce<number>((acc, s) => {
+
+  // This shouldn't happen, but it does because of an issue in the onTaskProgress callback returning undefined for newSteps between initialize and snapshot
+  if (!step) {
+    console.log("buildProgress.step is undefined", buildProgress);
+    return null;
+  }
+
+  const stepIndex = stepsArray.findIndex((s) => s.step === step);
+  const currentStep = stepsArray[stepIndex];
+  const previousStepsWeight = stepsArray.slice(0, stepIndex).reduce<number>((acc, s) => {
     return acc + s.weight;
   }, 0);
   const previousStepsPercentage = previousStepsWeight / totalWeight; // 0.xx
+
+  // Calculate the percentage of the current step if progress is available, otherwise default to 35%
   const percentage =
     (buildProgress.total ? buildProgress.progress / buildProgress.total : 0.35) *
-    (stepWeight / totalWeight);
+    (currentStep.weight / totalWeight);
 
   const totalPercentage = previousStepsPercentage + percentage;
 
