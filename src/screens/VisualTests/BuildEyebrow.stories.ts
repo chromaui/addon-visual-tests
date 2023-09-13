78122180d9839c537d8f8e9f090b6b3506b0bbb0
Gert Hengeveld <info@ghengeveld.nl>
2023-09-13 13:31:04 +0200

    Add stories for BuildProgressLabel

--- src/screens/VisualTests/BuildEyebrow.stories.ts
+++ src/screens/VisualTests/BuildEyebrow.stories.ts
@@ -2,9 +2,8 @@ import { action } from "@storybook/addon-actions";
 import type { Meta, StoryObj } from "@storybook/react";
 import { fireEvent, within } from "@storybook/testing-library";
 
-import { INITIAL_BUILD_PAYLOAD } from "../../buildSteps";
+import * as buildProgressStories from "../../components/BuildProgressLabel.stories";
 import { playAll } from "../../utils/playAll";
-import { withFigmaDesign } from "../../utils/withFigmaDesign";
 import { BuildEyebrow } from "./BuildEyebrow";
 
 const meta = {
@@ -25,97 +24,37 @@ const expandEyebrow = playAll(async ({ canvasElement }) => {
 });
 
 export const Initialize: Story = {
-  args: {
-    runningBuild: INITIAL_BUILD_PAYLOAD,
-  },
-  parameters: withFigmaDesign(
-    "https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2892-73423&mode=design&t=gIM40WT0324ynPQD-4"
-  ),
+  args: buildProgressStories.Initialize.args,
+  parameters: buildProgressStories.Initialize.parameters,
   play: expandEyebrow,
 };
 
 export const Build: Story = {
-  args: {
-    runningBuild: {
-      ...INITIAL_BUILD_PAYLOAD,
-      buildProgressPercentage: 8,
-      currentStep: "build",
-    },
-  },
-  parameters: withFigmaDesign(
-    "https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2892-73453&mode=design&t=gIM40WT0324ynPQD-4"
-  ),
+  args: buildProgressStories.Build.args,
+  parameters: buildProgressStories.Build.parameters,
   play: expandEyebrow,
 };
 
 export const Upload: Story = {
-  args: {
-    runningBuild: {
-      ...INITIAL_BUILD_PAYLOAD,
-      buildProgressPercentage: 50,
-      currentStep: "upload",
-      stepProgress: {
-        ...INITIAL_BUILD_PAYLOAD.stepProgress,
-        upload: {
-          startedAt: Date.now() - 3000,
-          numerator: 4_200_000,
-          denominator: 123_000_000,
-        },
-      },
-    },
-  },
-  parameters: withFigmaDesign(
-    "https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2935-71430&mode=design&t=gIM40WT0324ynPQD-4"
-  ),
+  args: buildProgressStories.Upload.args,
+  parameters: buildProgressStories.Upload.parameters,
   play: expandEyebrow,
 };
 
 export const Verify: Story = {
-  args: {
-    runningBuild: {
-      ...INITIAL_BUILD_PAYLOAD,
-      buildProgressPercentage: 75,
-      currentStep: "verify",
-    },
-  },
-  parameters: withFigmaDesign(
-    "https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2935-72020&mode=design&t=gIM40WT0324ynPQD-4"
-  ),
+  args: buildProgressStories.Verify.args,
+  parameters: buildProgressStories.Verify.parameters,
   play: expandEyebrow,
 };
 
 export const Snapshot: Story = {
-  args: {
-    runningBuild: {
-      ...INITIAL_BUILD_PAYLOAD,
-      buildProgressPercentage: 90,
-      currentStep: "snapshot",
-      stepProgress: {
-        ...INITIAL_BUILD_PAYLOAD.stepProgress,
-        snapshot: {
-          startedAt: Date.now() - 5000,
-          numerator: 25,
-          denominator: 50,
-        },
-      },
-    },
-  },
-  parameters: withFigmaDesign(
-    "https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2892-74603&mode=design&t=gIM40WT0324ynPQD-4"
-  ),
+  args: buildProgressStories.Snapshot.args,
+  parameters: buildProgressStories.Snapshot.parameters,
   play: expandEyebrow,
 };
 
 export const Complete: Story = {
-  args: {
-    runningBuild: {
-      ...INITIAL_BUILD_PAYLOAD,
-      currentStep: "complete",
-      buildProgressPercentage: 100,
-    },
-  },
-  parameters: withFigmaDesign(
-    "https://www.figma.com/file/GFEbCgCVDtbZhngULbw2gP/Visual-testing-in-Storybook?type=design&node-id=2892-74801&mode=design&t=gIM40WT0324ynPQD-4"
-  ),
+  args: buildProgressStories.Complete.args,
+  parameters: buildProgressStories.Complete.parameters,
   play: expandEyebrow,
 };
