276feb796c86534bf588ef0cdc41dba833ac3c57
Tom Coleman <tom@chromatic.com>
2023-09-18 22:17:05 +1000

    Add handler argument to `useChromaticDialog`
    Copied and adapted with help from @kasperpeulen from https://github.com/chromaui/addon-visual-tests/pull/100

--- src/utils/useChromaticDialog.ts
+++ src/utils/useChromaticDialog.ts
@@ -1,7 +1,45 @@
 import { useCallback, useEffect, useRef } from "react";
+import { z } from "zod";
 
-export const useChromaticDialog = () => {
+const dialogPayloadSchema = z.union([
+  z.object({ message: z.literal("login") }),
+  z.object({ message: z.literal("grant"), denied: z.boolean() }),
+  z.object({ message: z.literal("projectCreated"), projectId: z.string() }),
+]);
+
+type Schema = z.infer<typeof dialogPayloadSchema>;
+
+// This semi-magical code is to preserve the union as we map from Schema->Pairs[keyof Pairs]
+// Got the technique from https://stackoverflow.com/a/75805274
+type Pairs<K extends Schema["message"] = Schema["message"]> = {
+  [TMessage in K]: [TMessage, Omit<Extract<Schema, { message: TMessage }>, "message">];
+};
+
+function mapToPairs<K extends Schema["message"]>(
+  parsed: Extract<Schema, { message: K }>
+): Pairs<K>[K] {
+  return [parsed.message, parsed];
+}
+
+export type DialogHandler = (...args: Pairs[keyof Pairs]) => void;
+
+export const useChromaticDialog = (handler?: DialogHandler) => {
   const dialog = useRef<Window | null>();
+  const dialogOrigin = useRef<string>();
+
+  useEffect(() => {
+    const handleMessage = ({ origin, data }: MessageEvent) => {
+      if (origin === dialogOrigin.current) {
+        const parsed = dialogPayloadSchema.parse(data);
+
+        handler?.(...mapToPairs(parsed));
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => window.removeEventListener("message", handleMessage);
+  }, [handler]);
 
   return [
     useCallback((url: string) => {
@@ -18,7 +56,10 @@ export const useChromaticDialog = () => {
       } else {
         dialog.current = window.open(url, "_blank");
       }
+      const { origin } = new URL(url);
+      dialogOrigin.current = origin;
     }, []),
+
     useCallback(() => dialog.current?.close(), []),
   ] as const;
 };
