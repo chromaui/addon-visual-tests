78fc34316799ba6e6872d5d08e6d10f4fe55d31b
Gert Hengeveld <info@ghengeveld.nl>
2023-11-20 21:20:53 +0100

    Add SnapshotImageThumb component

---
+++ src/components/SnapshotImageThumb.tsx
@@ -0,0 +1,53 @@
+import { Icons } from "@storybook/components";
+import { styled } from "@storybook/theming";
+import React from "react";
+
+const Wrapper = styled.div<{ status?: "positive" }>(({ status, theme }) => ({
+  position: "relative",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: theme.background.content,
+  border: `1px solid ${status === "positive" ? theme.color.green : theme.color.border}`,
+  borderRadius: 5,
+  margin: 10,
+  padding: 2,
+  minHeight: 200,
+  minWidth: 200,
+
+  img: {
+    display: "block",
+    maxWidth: "100%",
+    borderRadius: 3,
+  },
+  svg: {
+    position: "absolute",
+    top: -10,
+    left: -10,
+    width: 24,
+    height: 24,
+    padding: 5,
+    color: theme.color.lightest,
+    borderRadius: "50%",
+    backgroundColor: theme.color.green,
+  },
+}));
+
+interface SnapshotImageThumbProps {
+  thumbnailUrl: string;
+  backgroundColor?: string;
+  status?: "positive";
+}
+
+export const SnapshotImageThumb = ({
+  backgroundColor,
+  status,
+  thumbnailUrl,
+}: SnapshotImageThumbProps) => {
+  return (
+    <Wrapper status={status} style={backgroundColor ? { backgroundColor } : {}}>
+      <img src={thumbnailUrl} alt="Snapshot thumbnail" />
+      {status === "positive" && <Icons icon="check" />}
+    </Wrapper>
+  );
+};
