374b01e2d2e7a60afe80e2d27e0c8abd38304b68
Jarel Fryer <jarelfryer@gmail.com>
2024-02-02 12:46:35 -0600

    AP-4129 adjust ordering and icons on Links

--- src/components/FooterMenu.tsx
+++ src/components/FooterMenu.tsx
@@ -1,9 +1,8 @@
-import { ChangedIcon, ChromaticIcon, EllipsisIcon, QuestionIcon, UserIcon } from "@storybook/icons";
+import { ChangedIcon, EllipsisIcon, QuestionIcon, ShareAltIcon, UserIcon } from "@storybook/icons";
 import { useChannel } from "@storybook/manager-api";
 import React from "react";
 
 import { REMOVE_ADDON } from "../constants";
-import { useProjectId } from "../utils/useProjectId";
 import { TooltipMenu } from "./TooltipMenu";
 
 interface FooterMenuProps {
@@ -13,40 +12,42 @@ interface FooterMenuProps {
 
 export const FooterMenu = ({ setAccessToken, projectId }: FooterMenuProps) => {
   const emit = useChannel({});
+  const links = [
+    {
+      id: "remove",
+      title: "Remove addon",
+      icon: <ChangedIcon aria-hidden />,
+      onClick: () => emit(REMOVE_ADDON),
+    },
+    {
+      id: "logout",
+      title: "Log out",
+      icon: <UserIcon aria-hidden />,
+      onClick: () => setAccessToken(null),
+    },
+    {
+      id: "learn",
+      title: "Learn about this addon",
+      icon: <QuestionIcon aria-hidden />,
+      href: "https://www.chromatic.com/docs/visual-testing-addon",
+      target: "_blank",
+    },
+    ...(projectId
+      ? [
+          {
+            id: "visit",
+            title: "Visit Project on Chromatic",
+            icon: <ShareAltIcon aria-hidden />,
+            href: projectId
+              ? `https://www.chromatic.com/builds?appId=${projectId?.split(":")[1]}`
+              : "https://www.chromatic.com/start",
+            target: "_blank",
+          },
+        ]
+      : []),
+  ];
   return (
-    <TooltipMenu
-      placement="top"
-      links={[
-        {
-          id: "logout",
-          title: "Log out",
-          icon: <UserIcon aria-hidden />,
-          onClick: () => setAccessToken(null),
-        },
-        {
-          id: "learn",
-          title: "Learn about this addon",
-          icon: <QuestionIcon aria-hidden />,
-          href: "https://www.chromatic.com/docs/visual-testing-addon",
-          target: "_blank",
-        },
-        {
-          id: "visit",
-          title: "Visit Project on Chromatic",
-          icon: <ChromaticIcon aria-hidden />,
-          href: projectId
-            ? `https://www.chromatic.com/builds?appId=${projectId?.split(":")[1]}`
-            : "https://www.chromatic.com/start",
-          target: "_blank",
-        },
-        {
-          id: "remove",
-          title: "Remove addon",
-          icon: <ChangedIcon aria-hidden />,
-          onClick: () => emit(REMOVE_ADDON),
-        },
-      ]}
-    >
+    <TooltipMenu placement="top" links={links}>
       <EllipsisIcon />
     </TooltipMenu>
   );
