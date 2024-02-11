7d72b51034a8c496d45c8a7391a3819927e9dc13
Gert Hengeveld <info@ghengeveld.nl>
2024-02-11 19:13:18 +0100

    wip

--- src/components/Screen.stories.tsx
+++ src/components/Screen.stories.tsx
@@ -1,6 +1,10 @@
+import { mockChannel } from "@storybook/manager-api";
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
+import React, { useEffect } from "react";
 
+import { CONFIG_INFO, CONFIG_INFO_DISMISSED } from "../constants";
+import { ConfigInfoPayload } from "../types";
+import { SharedState } from "../utils/SharedState";
 import { Screen } from "./Screen";
 
 const meta = {
@@ -14,3 +18,39 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const ConfigurationProblems = {
+  render: (args) => {
+    const state = SharedState.subscribe<ConfigInfoPayload>(CONFIG_INFO, mockChannel());
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      state.value = {
+        problems: { storybookBaseDir: "src/frontend" },
+        suggestions: { zip: true },
+      };
+      return () => {
+        state.value = null;
+      };
+    }, [state]);
+
+    return <Screen {...args} />;
+  },
+} satisfies StoryObj<typeof meta>;
+
+export const ConfigurationSuggestions = {
+  render: (args) => {
+    const state = SharedState.subscribe<ConfigInfoPayload>(CONFIG_INFO, mockChannel());
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      state.value = { suggestions: { zip: true } };
+      return () => {
+        state.value = null;
+        localStorage.removeItem(CONFIG_INFO_DISMISSED);
+      };
+    }, [state]);
+
+    return <Screen {...args} />;
+  },
+} satisfies StoryObj<typeof meta>;
--- src/screens/GitNotFound/GitNotFound.stories.tsx
+++ src/screens/GitNotFound/GitNotFound.stories.tsx
@@ -1,11 +1,6 @@
 // @ts-nocheck TODO: Address SB 8 type errors
-import { mockChannel } from "@storybook/manager-api";
 import type { Meta, StoryObj } from "@storybook/react";
-import React, { useEffect } from "react";
 
-import { CONFIG_INFO, CONFIG_INFO_DISMISSED } from "../../constants";
-import { ConfigInfoPayload } from "../../types";
-import { SharedState } from "../../utils/SharedState";
 import { GitNotFound } from "./GitNotFound";
 
 const meta = {
@@ -14,43 +9,7 @@ const meta = {
     gitInfoError: new Error("Git info not found"),
   },
 } satisfies Meta<typeof GitNotFound>;
-
-export const Default = {} satisfies StoryObj<typeof meta>;
-
-export const ConfigurationProblems = {
-  render: (args) => {
-    const state = SharedState.subscribe<ConfigInfoPayload>(CONFIG_INFO, mockChannel());
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      state.value = {
-        problems: { storybookBaseDir: "src/frontend" },
-        suggestions: { zip: true },
-      };
-      return () => {
-        state.value = null;
-      };
-    }, [state]);
-
-    return <GitNotFound {...args} />;
-  },
-} satisfies StoryObj<typeof meta>;
-
-export const ConfigurationSuggestions = {
-  render: (args) => {
-    const state = SharedState.subscribe<ConfigInfoPayload>(CONFIG_INFO, mockChannel());
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      state.value = { suggestions: { zip: true } };
-      return () => {
-        state.value = null;
-        localStorage.removeItem(CONFIG_INFO_DISMISSED);
-      };
-    }, [state]);
-
-    return <GitNotFound {...args} />;
-  },
-} satisfies StoryObj<typeof meta>;
-
 export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
