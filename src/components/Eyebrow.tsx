57a015cbe02175bebda32f5304b5eea80cef69ae
domyen <dom@hichroma.com>
2024-02-12 19:14:35 +0100

    Remove border from Eyebrow

--- src/components/Eyebrow.tsx
+++ src/components/Eyebrow.tsx
@@ -2,7 +2,6 @@ import { styled } from "@storybook/theming";
 
 export const Eyebrow = styled.div(({ theme }) => ({
   background: theme.background.app,
-  borderBottom: `1px solid ${theme.appBorderColor}`,
   padding: "10px 15px",
   lineHeight: "20px",
   color: theme.color.defaultText,
