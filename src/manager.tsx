7c7d43245d5d57044f93140f7b10e54380b15ef7
Gert Hengeveld <info@ghengeveld.nl>
2024-03-05 15:23:34 +0100

    Show notification when connection to server is lost

--- src/manager.tsx
+++ src/manager.tsx
@@ -1,4 +1,6 @@
-import { addons, types } from "@storybook/manager-api";
+import { addons, type API } from "@storybook/manager-api";
+import { color } from "@storybook/theming";
+import { Addon_TypesEnum } from "@storybook/types";
 import React from "react";
 
 import { ADDON_ID, PANEL_ID, SIDEBAR_BOTTOM_ID, SIDEBAR_TOP_ID } from "./constants";
@@ -6,21 +8,48 @@ import { Panel } from "./Panel";
 import { SidebarBottom } from "./SidebarBottom";
 import { SidebarTop } from "./SidebarTop";
 
+let heartbeatTimeout: NodeJS.Timeout;
+const expectHeartbeat = (api: API) => {
+  heartbeatTimeout = setTimeout(() => expectHeartbeat(api), 30000);
+};
+
 addons.register(ADDON_ID, (api) => {
   addons.add(PANEL_ID, {
-    type: types.PANEL,
+    type: Addon_TypesEnum.PANEL,
     title: "Visual Tests",
     match: ({ viewMode }) => viewMode === "story",
     render: ({ active }) => <Panel active={!!active} api={api} />,
   });
 
   addons.add(SIDEBAR_TOP_ID, {
-    type: types.experimental_SIDEBAR_TOP,
+    type: Addon_TypesEnum.experimental_SIDEBAR_TOP,
     render: () => <SidebarTop api={api} />,
   });
 
   addons.add(SIDEBAR_BOTTOM_ID, {
-    type: types.experimental_SIDEBAR_BOTTOM,
+    type: Addon_TypesEnum.experimental_SIDEBAR_BOTTOM,
     render: () => <SidebarBottom api={api} />,
+  });
+
+  const channel = api.getChannel();
+  if (!channel) return;
+
+  channel.on(`${ADDON_ID}/heartbeat`, () => {
+    clearTimeout(heartbeatTimeout);
+    heartbeatTimeout = setTimeout(() => {
+      api.addNotification({
+        id: `${ADDON_ID}/connection-lost`,
+        content: {
+          headline: "Connection lost",
+          subHeadline: "Lost connection to the Storybook server. Try refreshing the page.",
+        },
+        icon: {
+          name: "failed",
+          color: color.negative,
+        },
+        // @ts-expect-error SB needs a proper API for no link
+        link: undefined,
+      });
+    }, 3000);
   });
 });
