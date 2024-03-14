de3a9259800e78213070fa73a24df0828067179b
Andrew Ortwein <andrewortwein@icloud.com>
2024-03-14 01:03:15 -0400

    Split out initial selection logic and add tests, fix stateful selection

--- src/utils/useTests.ts
+++ src/utils/useTests.ts
@@ -1,14 +1,8 @@
 import { useGlobals, useGlobalTypes } from "@storybook/manager-api";
-import { useCallback, useMemo, useRef, useState } from "react";
+import { useCallback, useState } from "react";
 
 import { SELECTED_BROWSER_ID, SELECTED_MODE_NAME } from "../constants";
-import {
-  BrowserInfo,
-  ComparisonResult,
-  StoryTestFieldsFragment,
-  TestMode,
-  TestStatus,
-} from "../gql/graphql";
+import { BrowserInfo, ComparisonResult, StoryTestFieldsFragment, TestMode } from "../gql/graphql";
 import { useSharedState } from "./useSharedState";
 
 type BrowserData = Pick<BrowserInfo, "id" | "key" | "name">;
@@ -27,16 +21,47 @@ const hasChanges = ({ result }: StoryTestFieldsFragment["comparisons"][number]) 
   result !== ComparisonResult.Equal && result !== ComparisonResult.Fixed;
 
 /**
- * Pick a single test+comparison (by viewport+browser) from a set of tests
- * for the same story.
- *
- * Select the initial test mode based on the following criteria (in order of priority):
- * 1. The first test with changes detected and the mode name matches the one previously selected
+ * Select the initial test based on the following criteria (in order of priority):
+ * 1. The first test with changes detected and the mode name matches the global one
  * 2. The first test with changes detected
- * 3. The first test where the mode name matches the one previously selected
+ * 3. The first test where the mode name matches the global one
  * 4. The first test
  */
+export const getMostUsefulTest = (
+  tests: StoryTestFieldsFragment[],
+  modeName?: ModeData["name"]
+): StoryTestFieldsFragment => {
+  const changedTests = tests.filter((test) => test.comparisons.some(hasChanges));
+  const candidateTests = changedTests.length ? changedTests : tests;
+  const test = candidateTests.find((t) => t.mode.name === modeName) || candidateTests[0];
+  return test;
+};
+
+/**
+ * Select the initial comparison based on the following criteria (in order of priority):
+ * 1. The comparison with changes detected and the browser id matches the global one
+ * 2. The first comparison with changes detected
+ * 3. The first comparison where the browser id matches the global one
+ * 4. The first comparison
+ */
+export const getMostUsefulComparison = (
+  comparisons: StoryTestFieldsFragment["comparisons"],
+  browserId?: BrowserData["id"]
+): StoryTestFieldsFragment["comparisons"][number] => {
+  const changedComparisons = comparisons.filter(hasChanges);
+  const candidateComparisons = changedComparisons.length ? changedComparisons : comparisons;
+  const comparison =
+    candidateComparisons.find((c) => c.browser.id === browserId) || candidateComparisons[0];
+  return comparison;
+};
+
+/**
+ * Pick a single test+comparison (by viewport+browser) from a set of tests
+ * for the same story.
+ */
 export function useTests(tests: StoryTestFieldsFragment[]) {
+  // console.log("rendering useTests");
+  const [initialRender, setInitialRender] = useState(true);
   const themeType = useGlobalValue("theme")[1];
 
   const [globalSelectedModeName, setGlobalSelectedModeName] =
@@ -44,37 +69,28 @@ export function useTests(tests: StoryTestFieldsFragment[]) {
   const [globalSelectedBrowserId, setGlobalSelectedBrowserId] =
     useSharedState<string>(SELECTED_BROWSER_ID);
 
-  const selectedModeName = useRef(globalSelectedModeName);
-  const selectedBrowserId = useRef(globalSelectedBrowserId);
+  // TODO: how should we actually handle when there are no tests?
+  // if (!tests.length) {
+  //   return {};
+  // }
 
-  if (tests?.length && (!selectedModeName.current || !selectedBrowserId.current)) {
-    const changedTests = tests.filter((test) => test.comparisons.some(hasChanges));
-    const candidateTests = changedTests.length ? changedTests : tests;
-    const selectedTest =
-      candidateTests.find((t) => t.mode.name === globalSelectedModeName) || candidateTests[0];
-
-    const changedComparisons = selectedTest.comparisons.filter(hasChanges);
-    const candidateComparisons = changedComparisons.length
-      ? changedComparisons
-      : selectedTest.comparisons;
-    const selectedComparison =
-      candidateComparisons.find((c) => c.browser.id === globalSelectedBrowserId) ||
-      candidateComparisons[0];
-
-    selectedModeName.current = selectedTest.mode.name;
-    selectedBrowserId.current = selectedComparison.browser.id;
-  }
-
-  const selectedTest = tests.find(({ mode }) => mode.name === selectedModeName.current) || tests[0];
-  const selectedComparison =
-    selectedTest?.comparisons.find(({ browser }) => browser.id === selectedBrowserId.current) ||
-    selectedTest?.comparisons[0];
+  const selectedTest = initialRender
+    ? getMostUsefulTest(tests, globalSelectedModeName)
+    : tests.find(({ mode }) => mode.name === globalSelectedModeName);
+  // console.log("selectedTest", selectedTest);
+  const selectedComparison = initialRender
+    ? getMostUsefulComparison(selectedTest?.comparisons, globalSelectedBrowserId)
+    : selectedTest?.comparisons.find(({ browser }) => browser.id === globalSelectedBrowserId);
 
   if (selectedTest?.mode.name !== globalSelectedModeName) {
     setGlobalSelectedModeName(selectedTest?.mode.name);
   }
   if (selectedComparison?.browser.id !== globalSelectedBrowserId) {
     setGlobalSelectedBrowserId(selectedComparison?.browser.id);
+  }
+
+  if (initialRender) {
+    setInitialRender(false);
   }
 
   return {
