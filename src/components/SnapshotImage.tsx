dae268363706e21e92904149b5fb0b1e1eb458ed
Gert Hengeveld <info@ghengeveld.nl>
2024-05-16 16:28:16 +0200

    More obvious pulsation effect

--- src/components/SnapshotImage.tsx
+++ src/components/SnapshotImage.tsx
@@ -62,9 +62,9 @@ const StyledStack = styled(Stack)(({ theme }) => ({
 }));
 
 const pulsate = keyframes({
-  "0%": { opacity: 0.7 },
+  "0%": { opacity: 0.5 },
   "50%": { opacity: 1 },
-  "100%": { opacity: 0.7 },
+  "100%": { opacity: 0.5 },
 });
 
 const Image = styled.img<{ pulsating?: boolean }>(({ pulsating }) => ({
