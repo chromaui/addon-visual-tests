0c4b8e53d6d3136e044355e856167306b6ec8fa9
Norbert de Langen <ndelangen@me.com>
2025-02-27 13:18:19 +0100

    cleanup

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,8 +1,8 @@
 export default {
-  "printWidth": 100,
-  "tabWidth": 2,
-  "semi": true,
-  "singleQuote": true,
-  "trailingComma": "es5",
-  "bracketSpacing": true,
-}
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+};
