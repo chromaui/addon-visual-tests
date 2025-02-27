f004475ed5e58fca069091d332518f0a01915fa8
Norbert de Langen <ndelangen@me.com>
2025-02-27 11:47:24 +0100

    ignore .js files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,58 +1,67 @@
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-    ...compat.extends('plugin:@typescript-eslint/recommended', "plugin:react-hooks/recommended", "plugin:storybook/recommended", "plugin:prettier/recommended"),
-    {
-        plugins: {
-            "simple-import-sort": simpleImportSort,
-        },
-
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 5,
-            sourceType: "script",
-
-            parserOptions: {
-                project: ["./tsconfig.json"],
-            },
-        },
-
-        rules: {
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "@typescript-eslint/no-explicit-any": "warn",
-            "@typescript-eslint/no-unused-vars": "warn",
-            "eslint-comments/disable-enable-pair": "off",
-            "simple-import-sort/imports": "error",
-            "simple-import-sort/exports": "error",
-            "import/named": "off",
-            "import/order": "off",
-
-            // "import/no-extraneous-dependencies": ["error", {
-            //     devDependencies: true,
-            // }],
-
-            "@typescript-eslint/ban-ts-comment": ["error", {
-                "ts-expect-error": "allow-with-description",
-                "ts-ignore": "allow-with-description",
-                "ts-nocheck": "allow-with-description",
-                "ts-check": "allow-with-description",
-                minimumDescriptionLength: 3,
-            }],
-
-            "jest/no-deprecated-functions": "off",
-        },
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
     },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'eslint-comments/disable-enable-pair': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/named': 'off',
+      'import/order': 'off',
+
+      // "import/no-extraneous-dependencies": ["error", {
+      //     devDependencies: true,
+      // }],
+
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-expect-error': 'allow-with-description',
+          'ts-ignore': 'allow-with-description',
+          'ts-nocheck': 'allow-with-description',
+          'ts-check': 'allow-with-description',
+          minimumDescriptionLength: 3,
+        },
+      ],
+
+      'jest/no-deprecated-functions': 'off',
+    },
+  },
 ];
