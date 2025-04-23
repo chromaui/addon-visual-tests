4224e77ec131973492afa0d906678acfe2755192
Yann Braga <yannbf@gmail.com>
2025-04-23 12:11:33 +0200

    Fix: Clear testing status on logout

--- src/components/FooterMenu.tsx
+++ src/components/FooterMenu.tsx
@@ -1,8 +1,9 @@
 import { CogIcon, EllipsisIcon, QuestionIcon, ShareAltIcon, UserIcon } from '@storybook/icons';
 import React from 'react';
+import { experimental_getStatusStore } from 'storybook/manager-api';
 
 import { useAuthState } from '../AuthContext';
-import { PROJECT_INFO } from '../constants';
+import { ADDON_ID, PROJECT_INFO } from '../constants';
 import { useControlsDispatch } from '../screens/VisualTests/ControlsContext';
 import { ProjectInfoPayload } from '../types';
 import { useSharedState } from '../utils/useSharedState';
@@ -12,6 +13,7 @@ export const FooterMenu = () => {
   const { accessToken, setAccessToken } = useAuthState();
   const { toggleConfig } = useControlsDispatch();
   const [projectInfo] = useSharedState<ProjectInfoPayload>(PROJECT_INFO);
+  const statusStore = experimental_getStatusStore(ADDON_ID);
   const { projectId } = projectInfo || {};
   const links = [
     {
@@ -30,26 +32,29 @@ export const FooterMenu = () => {
 
     ...(projectId
       ? [
-          {
-            id: 'visit',
-            title: 'View project on Chromatic',
-            icon: <ShareAltIcon aria-hidden />,
-            href: projectId
-              ? `https://www.chromatic.com/builds?appId=${projectId?.split(':')[1]}`
-              : 'https://www.chromatic.com/start',
-            target: '_blank',
-          },
-        ]
+        {
+          id: 'visit',
+          title: 'View project on Chromatic',
+          icon: <ShareAltIcon aria-hidden />,
+          href: projectId
+            ? `https://www.chromatic.com/builds?appId=${projectId?.split(':')[1]}`
+            : 'https://www.chromatic.com/start',
+          target: '_blank',
+        },
+      ]
       : []),
     ...(accessToken
       ? [
-          {
-            id: 'logout',
-            title: 'Log out',
-            icon: <UserIcon aria-hidden />,
-            onClick: () => setAccessToken(null),
+        {
+          id: 'logout',
+          title: 'Log out',
+          icon: <UserIcon aria-hidden />,
+          onClick: () => {
+            statusStore.unset();
+            setAccessToken(null);
           },
-        ]
+        },
+      ]
       : []),
   ];
   return (
