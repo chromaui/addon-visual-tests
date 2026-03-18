8a4e180bcc96575165453a83741a5f02f866fd83
Gert Hengeveld <info@ghengeveld.nl>
2026-03-18 10:51:35 +0100

    Add tests and stories

--- src/components/GlobalIgnoreToggle.tsx
+++ src/components/GlobalIgnoreToggle.tsx
@@ -5,14 +5,19 @@ import { useChannel, useGlobals } from 'storybook/manager-api';
 
 import { HIGHLIGHT_IGNORED_COUNT, HIGHLIGHT_IGNORED_PARAM } from '../constants';
 
-export const GlobalIgnoreToggle = () => {
-  const [globals, updateGlobals, storyGlobals] = useGlobals();
-  const [ignoreCount, setIgnoreCount] = useState(0);
-  const enabled = !!globals[HIGHLIGHT_IGNORED_PARAM];
-  const locked = HIGHLIGHT_IGNORED_PARAM in storyGlobals;
+type GlobalIgnoreToggleButtonProps = {
+  enabled: boolean;
+  ignoreCount: number;
+  locked: boolean;
+  onToggle: () => void;
+};
 
-  useChannel({ [HIGHLIGHT_IGNORED_COUNT]: setIgnoreCount }, []);
-
+export const GlobalIgnoreToggleButton = ({
+  enabled,
+  ignoreCount,
+  locked,
+  onToggle,
+}: GlobalIgnoreToggleButtonProps) => {
   return ignoreCount === 0 ? null : (
     <IconButton
       key={HIGHLIGHT_IGNORED_PARAM}
@@ -25,12 +30,30 @@ export const GlobalIgnoreToggle = () => {
       padding="small"
       variant="ghost"
       disabled={locked}
-      onClick={() =>
-        updateGlobals({ [HIGHLIGHT_IGNORED_PARAM]: !globals[HIGHLIGHT_IGNORED_PARAM] })
-      }
+      onClick={onToggle}
     >
       <ContrastIgnoredIcon />
       {ignoreCount}
     </IconButton>
+  );
+};
+
+export const GlobalIgnoreToggle = () => {
+  const [globals, updateGlobals, storyGlobals] = useGlobals();
+  const [ignoreCount, setIgnoreCount] = useState(0);
+  const enabled = !!globals[HIGHLIGHT_IGNORED_PARAM];
+  const locked = HIGHLIGHT_IGNORED_PARAM in storyGlobals;
+
+  useChannel({ [HIGHLIGHT_IGNORED_COUNT]: setIgnoreCount }, []);
+
+  return (
+    <GlobalIgnoreToggleButton
+      enabled={enabled}
+      ignoreCount={ignoreCount}
+      locked={locked}
+      onToggle={() =>
+        updateGlobals({ [HIGHLIGHT_IGNORED_PARAM]: !globals[HIGHLIGHT_IGNORED_PARAM] })
+      }
+    />
   );
 };
